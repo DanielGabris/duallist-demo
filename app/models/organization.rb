6856e57ac3715f629eab7a4c771db4d0c90cc8fa
Daniel Gabris <softabroad71@outlook.com>
2016-02-18 14:48:02 +0100

    change

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,4 @@
 class Organization < ActiveRecord::Base
-  has_many :employees, :inverse_of => :organization
+  has_many :employees
   accepts_nested_attributes_for :employees, :allow_destroy => true
 end
